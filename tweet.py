870a7a3d910876af28fe7e5fa2c1fe48452187eb
gabe <gabe@signalnine.net>
2022-09-29 17:44:25 -0600

    add prompt in image alt text

--- tweet.py
+++ tweet.py
@@ -4,7 +4,12 @@ import tweepy
 from dotenv import load_dotenv
 import os
 import sys
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--filename", type=str)
+parser.add_argument("--prompt", type=str)
+args = parser.parse_args()
 load_dotenv()
 
 # Authenticate to Twitter
@@ -15,8 +20,8 @@ auth.set_access_token(os.getenv("TOKEN"), os.getenv("TOKENSECRET"))
 api = tweepy.API(auth)
 
 # Create a tweet
-media = api.media_upload(sys.argv[1])
-
+media = api.media_upload(args.filename)
+alt_text = api.create_media_metadata(media.media_id, args.prompt)
 tweet = ""
 api.update_status(status=tweet, media_ids=[media.media_id])
 
--- tweet.sh
+++ tweet.sh
@@ -3,5 +3,6 @@
 CONTAINER=$(/snap/bin/docker ps | grep miniconda | awk '{print $1}')
 /snap/bin/docker exec -i $CONTAINER /sd/run.sh
 IMAGE="$(ls /home/gabe/stable-diffusion/outputs/*.png -t | head -n1)"
+PROMPT="$(echo $IMAGE | awk -F/ '{ print $6 }' | sed 's/.\{4\}$//')"
 /home/gabe/realesrgan/realesrgan-ncnn-vulkan -i "$IMAGE" -o /tmp/upscaled.png
-python3 /home/gabe/lofi-imgen/tweet.py /tmp/upscaled.png
+python3 /home/gabe/lofi-imgen/tweet.py --filename /tmp/upscaled.png --prompt "$PROMPT"
